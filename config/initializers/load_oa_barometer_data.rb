209c560fdf3d5ac0646f23a7f723b8cf41739817
Christian Orthmann <christian.orthmann@hotmail.com>
2013-11-11 17:25:55 +0100

    added basic header, footer and journal-top-chart

--- config/initializers/load_oa_barometer_data.rb
+++ config/initializers/load_oa_barometer_data.rb
@@ -6,3 +6,11 @@ else
   BAROMETER_JSON = ActiveSupport::JSON.decode(File.read(File.join(Rails.root, "config", "oa_barometer.json")))
 end
 
+if Rails.env == 'test' and File.exists?("config/journal_top.test.json")
+  TOP_JSON = ActiveSupport::JSON.decode(File.read(File.join(Rails.root, "config", "journal_top.test.json")))
+elsif File.exists?("config/journal_top.local.json")
+  TOP_JSON = ActiveSupport::JSON.decode(File.read(File.join(Rails.root, "config", "journal_top.local.json")))
+else
+  TOP_JSON = ActiveSupport::JSON.decode(File.read(File.join(Rails.root, "config", "journal_top.json")))
+end
+
