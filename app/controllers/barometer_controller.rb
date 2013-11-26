202e2f42e53e39b9164551c2ae0af7d17ed92807
Christian Orthmann <christian.orthmann@hotmail.com>
2013-11-26 12:17:59 +0100

    added universities and research area charts and more

--- app/controllers/barometer_controller.rb
+++ app/controllers/barometer_controller.rb
@@ -1,6 +1,6 @@
 class BarometerController < ApplicationController
   before_filter :set_instance_variables, :only => [:index]
-  attr_accessor :selected_item, :menu_items, :selected_topic, :selected_institution
+  attr_accessor :selected_item, :menu_items, :menu_items_universities, :selected_topic, :selected_institution,:selected_scope_universities
   
   def index
     
@@ -10,6 +10,9 @@ class BarometerController < ApplicationController
     @menu_items = ["Journals", "Topics", "Institutions"]
     @selected_topic = (@selected_topic.nil? ? TOP_JSON["topics"].keys()[0] : @selected_topic)
     @selected_institution = (@selected_institution.nil? ? TOP_JSON["institutions"].keys()[0] : @selected_institution)
+    
+    @menu_items_universities = ["Total", "OA", "Not OA"]
+    @selected_scope_universities = (@selected_scope_universities.nil? ? @menu_items_universities[0] : @menu_items_universities)
   end
   
   def menu
@@ -17,7 +20,7 @@ class BarometerController < ApplicationController
     #puts params
     @selected_topic = params["sub-selector-topic"]
     @selected_institution = params["sub-selector-institution"]
-    
+    @selected_scope_universities = params["content-selector-universities"]
     respond_to do |format|
       format.html # show_rec_horses.html.erb
       format.js   # show_rec_horses.js.erb
