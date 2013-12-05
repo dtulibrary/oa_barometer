0708ca34bb6ec3454ee9e1f4605f3a728897341d
Christian Orthmann <christian.orthmann@hotmail.com>
2013-12-05 15:25:32 +0100

    added rounding in helper

--- app/helpers/barometer_helper.rb
+++ app/helpers/barometer_helper.rb
@@ -1,6 +1,6 @@
 module BarometerHelper
   def calculate_percentage(count, total, multiply=false)
-    return ((count.to_f/total.to_f)*100).to_i if multiply
+    return ((count.to_f/total.to_f)*100).round if multiply
     return (count.to_f/total.to_f)
   end
 end
